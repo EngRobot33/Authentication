0500113f654cb9892be1d0a2ec84bdfbae271390
EngRobot33 <hmdkhsrvee@gmail.com>
2022-07-18 23:08:48 +0430

    Add User model and User admin

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,52 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+
+def activate_user(modeladmin, request, queryset):
+    rows_updated = queryset.update(is_active=True)
+    if rows_updated == 1:
+        subject = 'user'
+    else:
+        subject = 'users'
+    modeladmin.message_user(request, '{} {} get activated'.format(rows_updated, subject))
+activate_user.short_description = 'Activate selected users'
+
+def deactivate_user(modeladmin, request, queryset):
+    rows_updated = queryset.update(is_active=False)
+    if rows_updated == 1:
+        subject = 'user'
+    else:
+        subject = 'users'
+    modeladmin.message_user(request, '{} {} get deactivated'.format(rows_updated, subject))
+deactivate_user.short_description = 'Deactivate selected users'
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('user', 'first_name_user', 'last_name_user', 'email_user', 'is_active')
+    search_fields = ['user', 'first_name_user', 'last_name_user', 'email_user',]
+    actions = [activate_user, deactivate_user,]
+
+    def first_name_user(self, obj):
+        if obj.user:
+            return obj.user.first_name
+        else:
+            return None
+    first_name_user.short_description = 'first name'
+
+    def last_name_user(self, obj):
+        if obj.user:
+            return obj.user.last_name
+        else:
+            return None
+    last_name_user.short_description = 'last name'
+    
+    def email_user(self, obj):
+        if obj.user:
+            return obj.user.email
+        else:
+            return None
+    email_user.short_description = 'email'
+
+
+from django.contrib.admin.models import LogEntry
+LogEntry.objects.all().delete()
--- user/models.py
+++ user/models.py
@@ -1,3 +1,12 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+User = settings.AUTH_USER_MODEL
+
+class User(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    is_active = models.BooleanField(default=False, verbose_name='active')
+
+    def __str__(self):
+        return f'User: {self.user.username}'
